539427e97bd2f7156ad91f0fd618cbddad68cce1
malb87 <v4n9gr6whg@privaterelay.appleid.com>
2025-11-10 01:39:08 +0100

    index.html

--- index.php
+++ index.php
@@ -1,39 +1,33 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>
-            <?php echo $_SERVER["HTTP_HOST"]; ?>
-        </title>
-<style type="text/css">
-/*<![CDATA[*/
- body {
-  text-align: center;
-  color: #336699;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 48px;
-  font-weight: bold;
-}
- h2.c1 {
-  color: #CC6600;
-  font-size: 24px;
-  font-weight: lighter;
-}
- p.c2 {
-  color: #CC6600;
-  font-size: 13px;
-  font-weight: lighter;
-}
-/*]]>*/
-</style>
-    </head>
-    <body>
-        <?php echo $_SERVER["HTTP_HOST"]; ?>
-        <h2 class="c1">
-            sito in costruzione
-        </h2>
-        <p class="c2">
-            hosting su piattaforma <?php echo $_SERVER["SERVER_SOFTWARE"]; ?>
-        </p>
-    </body>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>michelealbiani.it - Sito in costruzione</title>
+  <style>
+    body {
+      text-align: center;
+      color: #336699;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 48px;
+      font-weight: bold;
+      margin-top: 15%;
+    }
+    h2 {
+      color: #CC6600;
+      font-size: 24px;
+      font-weight: normal;
+    }
+    p {
+      color: #CC6600;
+      font-size: 14px;
+      font-weight: lighter;
+    }
+  </style>
+</head>
+<body>
+  michelealbiani.it
+  <h2>Sito in costruzione</h2>
+  <p>Hosting su Cloudflare Pages</p>
+</body>
 </html>
